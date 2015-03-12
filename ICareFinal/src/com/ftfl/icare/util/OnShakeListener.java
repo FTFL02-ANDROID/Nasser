e381f6a81071b8266ff453535621f36225bd3745
NomanUIU <noman.uiu@gmail.com>
2015-03-12 20:22:58 +0600

    ICare

---
+++ ICareFinal/src/com/ftfl/icare/util/OnShakeListener.java
@@ -0,0 +1,7 @@
+package com.ftfl.icare.util;
+
+public interface OnShakeListener {
+	
+	public void onShake();
+
+}
---
+++ ICareFinal/src/com/ftfl/icare/util/SpinnerItem.java
@@ -0,0 +1,29 @@
+package com.ftfl.icare.util;
+
+public class SpinnerItem {
+
+	int drawableResID;
+	String name;
+
+	public SpinnerItem(int drawableResID, String name) {
+		super();
+		this.drawableResID = drawableResID;
+		this.name = name;
+	}
+
+	public int getDrawableResID() {
+		return drawableResID;
+	}
+
+	public void setDrawableResID(int drawableResID) {
+		this.drawableResID = drawableResID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
